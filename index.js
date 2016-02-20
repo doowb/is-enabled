9871d4dab8075258ff31ad93096467093b4486c3
Brian Woodward <brian.woodward@gmail.com>
2016-02-20 14:41:59 -0500

    refactor info just a function call

--- index.js
+++ index.js
@@ -10,49 +10,44 @@
 var utils = require('./utils');
 
 /**
- * Create a plugin to add `isEnabled` to an application.
+ * Check if a property on an object is enabled and allow falsey values in
+ * the search key to indicate toggling the flag that's checked.
  *
+ * @param  {Object} `obj` Object to use when checking properties.
+ * @param  {String|Array} `keys` Key path to use.
  * @param  {Object} `options` Options to use to when checking for falsey values.
- * @return {Function} Plugin function to pass into `app.use`
+ * @param  {Boolean} `strict` When `true`, return `false` when `keys` is an empty string or array. (Defaults to returning `true`)
+ * @return {Boolean} If property value is considered "enabled" based on key path provided.
  * @api public
  */
 
-module.exports = function(options) {
-  var opts = utils.extend({prop: 'options', strict: false}, options);
-  var prop = opts.prop;
-  var strict = opts.strict;
+module.exports = function isEnabled(obj, keys, options) {
+  if (typeof obj !== 'object') {
+    throw new TypeError('expected "obj" to be an "object"');
+  }
 
-  return function(app) {
-    if (typeof app.isEnabled === 'function') return;
+  if (typeof keys === 'object' && !Array.isArray(keys)) {
+    options = keys;
+    keys = [];
+  }
 
-    this.define('isEnabled', function(keys) {
-      if (isEmpty(keys)) return !strict;
-      if (typeof keys === 'string') {
-        keys = keys.split('.');
-      }
+  var opts = utils.extend({strict: false}, options);
+  if (utils.isEmpty(keys)) return !opts.strict;
+  if (typeof keys === 'string') {
+    keys = keys.split('.');
+  }
 
-      // filter out falsey keys and flip the flag when falsey
-      var flag = true;
-      keys = keys.filter(function(m) {
-        if (utils.isFalsey(m, opts)) {
-          flag = !flag;
-          return false;
-        }
-        return true;
-      });
-
-      var key = keys[0];
-      if (isEmpty(key)) return flag;
-      return this[prop][key] === flag;
-    });
-
-    function isEmpty(val) {
-      if (!val) return true;
-      if (Array.isArray(val) && val.length === 0) {
-        return true;
-      }
+  // filter out falsey keys and flip the flag when falsey
+  var flag = true;
+  keys = keys.filter(function(m) {
+    if (utils.isFalsey(m, opts)) {
+      flag = !flag;
       return false;
     }
-  };
-};
+    return true;
+  });
 
+  var key = keys[0];
+  if (utils.isEmpty(key)) return flag;
+  return obj[key] === flag;
+};
