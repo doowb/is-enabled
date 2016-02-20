bb5a84c897a33425f483e6521b0ce2eda71d0be9
Brian Woodward <brian.woodward@gmail.com>
2016-02-19 23:38:53 -0500

    init

---
+++ utils.js
@@ -0,0 +1,42 @@
+/*!
+ * is-enabled <https://github.com/doowb/is-enabled>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var utils = require('lazy-cache')(require);
+var fn = require;
+require = utils;
+
+/**
+ * Lazily required module dependencies
+ */
+
+require('extend-shallow', 'extend');
+require('falsey');
+require = fn;
+
+// default keywords for falsey
+var keywords = ['none', 'nil', 'nope', 'no', 'not', 'nada', '0', 'false'];
+
+/**
+ * Check if a value is falsey with the given keywords.
+ *
+ * @param  {String} `val` Value to check for falsey
+ * @param  {Object} `options` Options to pass to falsey
+ * @return {Boolean} `true` if `val` is falsey
+ */
+
+utils.isFalsey = function(val, options) {
+  var opts = utils.extend({keywords: keywords}, options);
+  return utils.falsey(val, opts.keywords);
+};
+
+/**
+ * Expose `utils` modules
+ */
+
+module.exports = utils;
